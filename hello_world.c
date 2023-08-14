1e108540480c037614de262f7881fd9c0acf3f9a
PrasannaPrasanth <bprasanna.adhi@gmail.com>
2023-08-14 04:21:35 -0700

    Multiplecation completed

--- hello_world.c
+++ hello_world.c
@@ -1,9 +1,15 @@
 void main()
 {
     printf("%s","Hello World\n");
-    printf("%d",add(2,3));
+    printf("%d\n",add(2,3));
+    printf("%d",multiplcation(2,3));
 }
 int add(int a, int b)
 {
     return a+b;
 }
+int multiplcation(int a, int b)
+{
+    return a*b;
+}
+
